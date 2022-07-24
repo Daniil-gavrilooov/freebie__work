a7cd90680cf5fd86ed0f94490ef87eca306fea90
Daniil-gavrilooov <85822019+Daniil-gavrilooov@users.noreply.github.com>
2022-07-24 20:29:50 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.resources__slider').slick({
+        dots: true,
+        nextArrow: '<img class="slick-next" src="images/arrow_next.svg" alt="">',
+        prevArrow: '<img class="slick-prev" src="images/arrow_prew.svg" alt="">'
+    });
+})
