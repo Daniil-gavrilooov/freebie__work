612f9bbfefc9ba60b8cf86b8364f335e1606c140
gavrilov <daniil.gavrilooov@gmail.com>
2022-08-01 23:58:52 +0300

    finish section (questions)

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,45 @@
-$(function() {
+$(function () {
     $('.resources__slider').slick({
         dots: true,
         nextArrow: '<img class="slick-next" src="images/arrow_next.svg" alt="">',
         prevArrow: '<img class="slick-prev" src="images/arrow_prew.svg" alt="">'
     });
 })
-$(function() {
+$(function () {
     $('.coments__slider-inner').slick({
         dots: true,
         arrows: false,
         slideToShow: 2,
-        slideToScroll: 1,   
+        slideToScroll: 1,
     });
 })
+
+let questionsItembtn = document.querySelectorAll('.questions__title-btn')
+
+questionsItembtn.forEach(btn => btn.onclick = function () {
+    let btnParent = this.parentNode
+    let itemParent = btnParent.parentNode
+    let itemActive = itemParent.querySelector('.questions__item-info')
+    let titleActive = btnParent.querySelector('.questions__title-text')
+
+    this.classList.toggle('questions__title-btn--active')
+    itemActive.classList.toggle('questions__item-info--active')
+    titleActive.classList.toggle('questions__title-text--active')
+
+})
+
+
+
+
+function questionTemplate() {
+    return `<p class="questions__info-text">
+    Quidam vocibus eum ne, erat consectetuer voluptatibus ut nam. Eu usu vidit
+    tractatos,
+    vero tractatos ius an, in mel diceret persecuti.
+
+</p>
+
+<button class="questions__info-btn">
+    Go to documentation
+</button>`
+}
